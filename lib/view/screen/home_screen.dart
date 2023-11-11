d2a93b766635e18ffc40e1cd75c94c069c3f5d9e
Kasun Lakshitha <kasun2014@outlook.com>
2023-11-12 01:36:40 +0530

    Home screen

--- lib/view/screen/home_screen.dart
+++ lib/view/screen/home_screen.dart
@@ -5,9 +5,11 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: const Text('Home'),
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          child: const Text('Home'),
+        ),
       ),
     );
   }
